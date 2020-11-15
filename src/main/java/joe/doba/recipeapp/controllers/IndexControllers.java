47db7c4b6cf6456fb753a35cea6747ebecad54a3
Joe <luqiao19930111@gmail.com>
2020-11-14 19:54:06 -0500

    Add Notes and Recipe classes.

--- src/main/java/joe/doba/recipeapp/controllers/IndexControllers.java
+++ src/main/java/joe/doba/recipeapp/controllers/IndexControllers.java
@@ -1,4 +1,13 @@
 package joe.doba.recipeapp.controllers;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
 public class IndexControllers {
+    @RequestMapping({"", "/index"})
+    public String getIndexPage() {
+        System.out.println("Some message to say....ssssssssss");
+        return "index";
+    }
 }
